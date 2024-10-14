d7fa8a76436b67dc2dc5e504aec2370e446e4f18
Ignacio Garcia <ignaciogarcia@gmail.com>
2024-10-14 20:59:17 +0200

    Terminada la lectura de JSON

---
+++ src/main/java/com/ufv/restservice/rest_service/GreetingController.java
@@ -0,0 +1,26 @@
+package com.ufv.restservice.rest_service;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    @GetMapping("/users")
+    public ArrayList<User> users(){
+        JsonReader reader = new JsonReader();
+        ArrayList<User> userList = reader.readJsonFile("./src/main/resources/users.json");
+        return userList;
+    }
+}
+
